e06e7d6751220ca9d4f13423c0a4a44afd4edac5
Lorena Sambad Lema <lsambadlema@gmail.com>
2024-10-30 15:50:00 +0100

    Terminado Exercise31

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,30 @@
 package com.campusdual.classroom;
 
-public class Exercise31 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class Exercise31 {
     public static void main(String[] args) {
 
+        String filePath = "src/main/resources/lorem.txt";
+
+
+        readFileAndPrint(filePath);
+    }
+
+
+    public static void readFileAndPrint(String filePath) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line); // Imprime cada línea del archivo
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
     }
 }
+
